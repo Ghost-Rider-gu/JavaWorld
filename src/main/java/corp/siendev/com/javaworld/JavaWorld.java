7e095fdbad92ec847bde615d142d993a3e633459
Ghost-Rider <ghostrider.gu@gmail.com>
2020-11-21 22:04:46 +0300

    init and prepare the project

---
+++ src/main/java/corp/siendev/com/javaworld/JavaWorld.java
@@ -0,0 +1,11 @@
+package corp.siendev.com.javaworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWorld {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWorld.class, args);
+    }
+}
